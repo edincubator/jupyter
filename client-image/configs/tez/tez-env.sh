037dfab973bb26e52988407aeb923db5f6248f46
Mikel Emaldi <m.emaldi@gmail.com>
2019-09-10 14:38:12 +0200

    Adding tez configuration files

---
+++ client-image/configs/tez/tez-env.sh
@@ -0,0 +1,9 @@
+
+# Tez specific configuration
+export TEZ_CONF_DIR=/opt/tez/conf
+
+# Set HADOOP_HOME to point to a specific hadoop install directory
+export HADOOP_HOME=${HADOOP_HOME:-/usr}
+
+# The java implementation to use.
+export JAVA_HOME=/usr/jdk64/jdk1.8.0_112
